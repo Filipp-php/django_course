c60647ae2276469986a672b97340c2fc5d2466a5
Filipp-php <filipp.verh.olantsev@yandex.ru>
2022-12-09 23:12:14 +0500

    добавлен axios

--- awesomeproject/train-app/src/main.js
+++ awesomeproject/train-app/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios'
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
